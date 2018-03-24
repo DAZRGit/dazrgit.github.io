b9e2a0190cad066335f9a6714ac9c65319c1a584
Diana Alejandra Zapata Ramírez <da.zapata.ramirez@gmail.com>
2018-03-24 14:55:38 +0100

    moved node modules out of project

--- js/google_map.js
+++ js/google_map.js
@@ -8,7 +8,7 @@ function init() {
     var myLatlng = new google.maps.LatLng(11.997682, 121.915627);
     // 39.399872
     // -8.224454
-    
+
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
         zoom: 9,
@@ -26,24 +26,55 @@ function init() {
     // Get the HTML DOM element that will contain your map 
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
-
+    
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
+    var bounds = new google.maps.LatLngBounds();
     
     var addresses = ['Brooklyn'];
+    var json =[
+        {
+            "title":'Madrid',
+            "lat": 40.437530,
+            "lng": -3.637561,
+        },
+    ]
 
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x], null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: 'images/loc.png'
-            });
+    var marker;
+    var arr = [];
+    // for (var x = 0; x < addresses.length; x++) {
+    //     $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address=' + addresses[x], null, function (data) {
+    //         var p = data.results[0].geometry.location
+    //         var latlng = new google.maps.LatLng(p.lat, p.lng);
+    //         marker = new google.maps.Marker({
+    //             position: latlng,
+    //             map: map,
+    //             icon: 'images/loc.png'
+    //         });
+    //         arr.push(latLng);
+    //     });
 
+    // }    
+
+    for (var i = 0, length = json.length; i < length; i++) {
+        var data = json[i],
+        // Create the waypoints
+        var latLng = new google.maps.LatLng(data.lat, data.lng);
+        arr.push(latLng);
+
+        // Create the markers
+        var marker = new google.maps.Marker({
+            position: latLng,
+            map: map,
+            title: data.title,
+            icon: 'images/loc.png'
         });
     }
-    
+    //  Fit these bounds to the map
+    alert(arr);
+    for (var i = 0; i < arr.length; i++) {
+      bounds.extend(arr[i]);
+    }
+    map.fitBounds(bounds);
 }
 google.maps.event.addDomListener(window, 'load', init);
