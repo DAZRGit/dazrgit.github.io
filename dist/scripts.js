07788c4ca6e5dd7ceeeaa70d023f605ea93e15a7
Diana Alejandra Zapata Ramírez <da.zapata.ramirez@gmail.com>
2019-05-05 23:31:48 +0200

    New map and markers

--- dist/scripts.js
+++ dist/scripts.js
@@ -936,11 +936,19 @@ window.addEventListener('beforeinstallprompt', function(e){
 var L;
 
 function init() {
-    var mymap = L.map('mapid').setView([40.710250, -3.678752], 12);
-   
+    var mapElement = document.getElementById('mapid');
+    var jsonArray = "when";
+    var centerMap =[40.710250, -3.678752];
+    if(mapElement  == null)
+    {
+        mapElement = document.getElementById('mapid2');
+        jsonArray = "guest";
+        centerMap =[40.733143, -3.575531]
+        if(mapElement == null)
+            return;
+    }
 
-    if(mymap == null) return;
-    
+    var mymap = L.map(mapElement).setView(centerMap, 12);
     // Add layer to map
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -951,16 +959,35 @@ function init() {
     //Create and open InfoWindow.
    
     // JSON
-    var json = [{
-        "title": "Ceremonia - Basílica de Nuestra señora de Colmenar Viejo",
-        "lat":  40.657681, 
-        "lng":  -3.766327,
-    }, {
-        "title": "Fiesta - Finca La Dehesilla",
-        "lat": 40.725285,
-        "lng": -3.563794,
-    }];
-
+    var json =[];
+    if(jsonArray == "when")
+    {
+        json = [{
+            "title": "Ceremonia - Basílica de Nuestra señora de Colmenar Viejo",
+            "lat":  40.657681, 
+            "lng":  -3.766327,
+        }, {
+            "title": "Fiesta - Finca La Dehesilla",
+            "lat": 40.725285,
+            "lng": -3.563794,
+        }];
+    }
+    else{
+        json = [{
+            "title": "Fiesta - Finca La Dehesilla",
+            "lat": 40.725285,
+            "lng": -3.563794,
+        },
+        {
+            "title": "Hostal - Alcudia Sierra Norte",
+            "lat":  40.732751, 
+            "lng":  -3.582009,
+        }, {
+            "title": "Hostal - Azul",
+            "lat": 40.736115,
+            "lng": -3.584128,
+        }];
+    }
     var arr = [];
     // Loop the JSON
     for (var i = 0, length = json.length; i < length; i++) {
